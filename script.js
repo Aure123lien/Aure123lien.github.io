e8cd6983e7c61b3ad396f0d438ae65142b425a0c
Aurelien <winsaurelien@gmail.com>
2025-11-27 15:14:23 +0100

    développement d'une barre de navigation sur mon site

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-console.log("Portfolio de Wins Aurélien chargé !");
+// Permet la fonctionnalité de la barre de navigation //
 
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 60,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+console.log("Portfolio de Wins Aurélien chargé avec menu interactif !");
